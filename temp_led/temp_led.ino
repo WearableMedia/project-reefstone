d17d7cf13bd3891c67d55a3de4eb7e26506e7574
Jingwen <zjwwjz@live.cn>
2017-02-10 14:46:44 -0500

    init

---
+++ temp_led/temp_led.ino
@@ -0,0 +1,62 @@
+#include <Adafruit_CircuitPlayground.h>
+#include "Interval.h"
+Interval ledTimer;
+Interval tempTimer;
+
+float tempC, tempF;
+float threshold = 25.6;
+
+int brightness = 0;
+int speed = 1;
+
+bool toggle = true;
+
+void setup() {
+  Serial.begin(9600);
+  CircuitPlayground.begin();
+  for (int i = 0; i < 10; i++) {
+    CircuitPlayground.setPixelColor(i, 20 * i,    20 * i,    20 * i);
+  }
+  tempTimer.setInterval(checkTemp, 1000);
+  ledTimer.setInterval(fade, 100);
+
+}
+
+void loop() {
+  tempTimer.check();
+  ledTimer.check();
+  if (tempC < threshold) {
+    toggle = true;
+  } else {
+    toggle = false;
+  }
+}
+
+void fade() {
+  if (toggle == true) {
+    brightness += speed;
+    if (brightness >= 10 || brightness <= 0) {
+      speed = -1 * speed;
+    }
+    for (int i = 0; i < 10; i++) {
+      CircuitPlayground.setPixelColor(i, 20 * brightness,    20 * brightness,    20 * brightness);
+    }
+  } else {
+    for (int i = 0; i < 10; i++) {
+      CircuitPlayground.setPixelColor(i, 0,   0,    0);
+    }
+  }
+
+}
+
+void checkTemp() {
+  tempC = CircuitPlayground.temperature();
+  tempF = CircuitPlayground.temperatureF();
+
+  Serial.print("tempC: ");
+  Serial.print(tempC);
+  Serial.print("  tempF: ");
+  Serial.println(tempF);
+
+}
+
